941fc3358daf78037f3a33589af6f89328c4a3fa
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-07-30 18:01:15 +0800

    Post change to defer

--- 61-62-async-routing/before/client/src/pages/Post.jsx
+++ 61-62-async-routing/before/client/src/pages/Post.jsx
@@ -1,15 +1,19 @@
-import { Link, useLoaderData } from "react-router-dom"
+import { Await, defer, Link, useLoaderData } from "react-router-dom"
 import { getComments } from "../api/comments"
 import { getPost } from "../api/posts"
 import { getUser } from "../api/users"
+import { SimpleSkeletonText, Skeleton, SkeletonList } from "../components/Skeleton"
+import { Suspense } from "react"
 
 function Post() {
-  const { comments, post, user } = useLoaderData()
+  const { commentsPromise, postPromise, userPromise } = useLoaderData()
 
   return (
     <>
       <h1 className="page-title">
-        {post.title}
+        <SimpleSkeletonText resolve={postPromise}>
+          {post => post.title}
+        </SimpleSkeletonText>
         <div className="title-btns">
           <Link className="btn btn-outline" to="edit">
             Edit
@@ -17,20 +21,51 @@ function Post() {
         </div>
       </h1>
       <span className="page-subtitle">
-        By: <Link to={`/users/${user.id}`}>{user.name}</Link>
+        By:{" "}
+        <Suspense fallback={<Skeleton short inline />}>
+          <Await resolve={userPromise}>
+            {user => <Link to={`/users/${user.id}`}>{user.name}</Link>}
+          </Await>
+        </Suspense>
       </span>
-      <div>{post.body}</div>
+      <div>
+        <Suspense fallback={
+          <>
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+          </>
+        }>
+          <Await resolve={postPromise}>
+            {post => post.body}
+          </Await>
+        </Suspense>
+      </div>
 
       <h3 className="mt-4 mb-2">Comments</h3>
       <div className="card-stack">
-        {comments.map(comment => (
-          <div key={comment.id} className="card">
-            <div className="card-body">
-              <div className="text-sm mb-1">{comment.email}</div>
-              {comment.body}
+        <Suspense fallback={
+          <SkeletonList amount={5}>
+            <div className="card">
+              <div className="card-body">
+                <div className="text-sm mb-1"><Skeleton short /></div>
+                <Skeleton />
+                <Skeleton />
+              </div>
             </div>
-          </div>
-        ))}
+          </SkeletonList>
+        }>
+          <Await resolve={commentsPromise}>
+            {comments => comments.map(comment => (
+              <div key={comment.id} className="card">
+                <div className="card-body">
+                  <div className="text-sm mb-1">{comment.email}</div>
+                  {comment.body}
+                </div>
+              </div>
+            ))}
+          </Await>
+        </Suspense>
       </div>
     </>
   )
@@ -38,10 +73,12 @@ function Post() {
 
 async function loader({ request: { signal }, params: { postId } }) {
   const comments = getComments(postId, { signal })
-  const post = await getPost(postId, { signal })
-  const user = getUser(post.userId, { signal })
-
-  return { comments: await comments, post, user: await user }
+  const post = getPost(postId, { signal })
+  return defer({
+    commentsPromise: comments,
+    postPromise: post,
+    userPromise: post.then(post => getUser(post.userId, { signal })),
+  })
 }
 
 export const postRoute = {
